68fe0c194214db548fc6def7507e39a43b180304
bhenry3145 <bhenry3145@gmail.com>
2024-12-02 15:57:56 -0800

    working on grid

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,7 @@
+let daily = document.getElementById('daily');
+let weekly = document.getElementById('weekly');
+let monthly = document.getElementById('monthly');
+
 function getAllData(){
     fetch("../data.json")
     .then (response => response.json())
